17249552cdd6904c5be480866a64456a92000fa0
aAmmaraBabar <125147403+aAmmaraBabar@users.noreply.github.com>
2024-02-28 10:00:49 -0700

    .

--- customer_journey/lib/module/web_module/screens/homepage/homepage/homepage_widget/homepage_howitwork.dart
+++ customer_journey/lib/module/web_module/screens/homepage/homepage/homepage_widget/homepage_howitwork.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'package:customer_journey/constant/image_constant.dart';
+import 'package:customer_journey/constant/sized_constant.dart';
 import 'package:flutter/material.dart';
 
 class HowitWork extends StatelessWidget {
@@ -42,27 +43,145 @@ class HowitWork extends StatelessWidget {
                                   style: TextStyle(
                                       color: Colors.white, fontSize: 32),
                                 ),
-                                Container(
-                                    child: Row(
-                                  children: [
-                                    Container(
-                                      height: 15,
-                                      width: 15,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        border: Border.all(
-                                            color: Colors.grey.shade100),
-                                      ),
-                                    ),
-                                    Text(
-                                      'Create Your Route',
-                                      style: TextStyle(
-                                        color: Colors.grey.shade100,
-                                        fontSize: 16,
-                                      ),
-                                    ),
-                                  ],
-                                )),
+                                ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: steplist.length,
+                                    itemBuilder: (context, index) {
+                                      var text = steplist[index].text;
+                                      var subtext = steplist[index].subtext;
+                                      if (index == 2) {
+                                        return Container(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Container(
+                                                    height: 15,
+                                                    width: 15,
+                                                    decoration: BoxDecoration(
+                                                      shape: BoxShape.circle,
+                                                      border: Border.all(
+                                                          color: Colors
+                                                              .grey.shade100),
+                                                    ),
+                                                  ),
+                                                  padding45,
+                                                  Text(
+                                                    text,
+                                                    style: TextStyle(
+                                                      color:
+                                                          Colors.grey.shade100,
+                                                      fontSize: 16,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Row(
+                                                children: [
+                                                  const SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Container(
+                                                    height: 75,
+                                                    width: 1,
+                                                    color: Colors.black,
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 55,
+                                                  ),
+                                                  Text(
+                                                    subtext,
+                                                    style: TextStyle(
+                                                      color:
+                                                          Colors.grey.shade100,
+                                                      fontSize: 12,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        );
+                                      }
+                                      if (index == 1) {
+                                        return Container(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Container(
+                                                    height: 15,
+                                                    width: 15,
+                                                    decoration: BoxDecoration(
+                                                      shape: BoxShape.circle,
+                                                      border: Border.all(
+                                                          color: Colors
+                                                              .grey.shade100),
+                                                    ),
+                                                  ),
+                                                  padding45,
+                                                  Text(
+                                                    text,
+                                                    style: TextStyle(
+                                                      color:
+                                                          Colors.grey.shade100,
+                                                      fontSize: 16,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Row(
+                                                children: [
+                                                  const SizedBox(
+                                                    width: 5,
+                                                  ),
+                                                  Container(
+                                                    height: 75,
+                                                    width: 1,
+                                                    decoration: BoxDecoration(
+                                                      color:
+                                                          Colors.grey.shade600,
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              2),
+                                                    ),
+                                                    child: Column(
+                                                      children: [
+                                                        Container(
+                                                          width: 1,
+                                                          height: 30,
+                                                          color: Colors.white,
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                  const SizedBox(
+                                                    width: 55,
+                                                  ),
+                                                  Text(
+                                                    subtext,
+                                                    style: TextStyle(
+                                                      color:
+                                                          Colors.grey.shade100,
+                                                      fontSize: 12,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        );
+                                      }
+
+                                      return StepperWidget(
+                                        text: text,
+                                        subtext: subtext,
+                                      );
+                                    })
                               ],
                             ),
                           )),
@@ -77,5 +196,85 @@ class HowitWork extends StatelessWidget {
             ],
           ),
         ));
+  }
+}
+
+class StepperModel {
+  final String text;
+  final String subtext;
+  StepperModel({required this.text, required this.subtext});
+}
+
+List<StepperModel> steplist = [
+  StepperModel(
+      text: 'Create your route',
+      subtext:
+          "Enter your pickup & dropoff locations or the number of hours\nyou wish to book a car and driver for"),
+  StepperModel(
+      text: 'Choose Vehicle For You',
+      subtext:
+          'On the day of your ride, you will receive two emai\nand SMS updates - one informing you that.'),
+  StepperModel(
+      text: 'Enjoy The Journey',
+      subtext:
+          'After your ride has taken place, we would appreciate it\nif you could rate your car and driver.'),
+];
+
+class StepperWidget extends StatelessWidget {
+  const StepperWidget({super.key, required this.text, required this.subtext});
+  final String text;
+  final String subtext;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 15,
+                width: 15,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.grey.shade100),
+                ),
+              ),
+              padding45,
+              Text(
+                text,
+                style: TextStyle(
+                  color: Colors.grey.shade100,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 5,
+              ),
+              Container(
+                height: 75,
+                width: 1,
+                color: Colors.grey.shade600,
+              ),
+              const SizedBox(
+                width: 55,
+              ),
+              Text(
+                subtext,
+                style: TextStyle(
+                  color: Colors.grey.shade100,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
